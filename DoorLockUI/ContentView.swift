ef233a9383d1b7d294507057866e82f82d2a46a5
jinjio <35422448+jinjio@users.noreply.github.com>
2022-09-27 09:58:38 +0900

    Initial Commit

---
+++ DoorLockUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DoorLockUI
+//
+//  Created by 진지오 on 2022/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoorLockUI/DoorLockUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorLockUIApp.swift
+//  DoorLockUI
+//
+//  Created by 진지오 on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct DoorLockUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
